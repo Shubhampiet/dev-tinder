f1e15c01cd236f1ea1a97a475bc1e2273e5251c4
Shubham Singh <shubham.itpiet@gmail.com>
2026-06-26 19:45:14 +0530

    profile route added

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,14 +1,16 @@
 const express = require("express");
-const authRoutes = require("./routes/auth.routes");
 const cookieParser = require("cookie-parser");
+
+const authRoutes = require("./routes/auth.routes");
+const profileRoutes = require("./routes/profile.routes");
 
 const app = express();
 app.use(cookieParser());
-
 //middlewares
 app.use(express.json());
 
 //routes
 app.use("/api/auth", authRoutes);
+app.use("/api/profile", profileRoutes);
 
 module.exports = app;
