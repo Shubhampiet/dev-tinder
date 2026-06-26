4b6fc76916b207aa3c6db8958e3d06b423443b81
Shubham Singh <shubham.itpiet@gmail.com>
2026-06-26 18:25:54 +0530

    auth controller

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,117 @@
+const User = require("../models/user.model");
+const generateToken = require("../utils/generateToken");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const validateSignUpData = require("../utils/validate");
+
+const signUp = async (req, res) => {
+    const { firstName, lastName, email, password } = req.body;
+
+  try {
+
+    // Validate the input data
+    const { valid, errors } = validateSignUpData({
+      firstName,
+      lastName,
+      email,
+      password,
+    });
+    if (!valid) {
+      return res.status(400).json({ errors });
+    }
+
+ 
+
+    // Check if the user already exists
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ error: "User already exists" });
+    }
+
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Create new user
+    const user = await User.create({
+      firstName,
+      lastName,
+      email,
+      password: hashedPassword,
+    });
+
+    // Generate token
+    const token = generateToken(user._id);
+
+    res.cookie("token", token, {
+      httpOnly: true,
+      maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+    });
+
+    res.status(201).json({
+      message: "User registered successfully",
+      user: {
+        id: user._id,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        email: user.email,
+      },
+    });
+  } catch (error) {
+    console.error("Error during sign up:", error);
+    res.status(500).json({ error: "Server error" });
+  }
+};
+
+//Sign In Controller
+const signIn = async (req, res) => {
+  const { email, password } = req.body;
+
+  // Check if user exists
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ error: "User not found" });
+    }
+
+    // Compare password
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ error: "Invalid email or password" });
+    }
+
+    // Generate token
+    const token = generateToken(user._id);
+
+    res.cookie("token", token, {
+      httpOnly: true,
+      maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+    });
+    res.status(200).json({
+      message: "User signed in successfully",
+      user: {
+        id: user._id,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        email: user.email,
+      },
+    });
+  } catch (error) {
+    console.error("Error during sign in:", error);
+    res.status(500).json({ error: "Server error" });
+  }
+};
+
+//logout controller
+const logout = (req, res) => {
+  res.clearCookie("token");
+res.status(200).json({
+    message: "User logged out successfully"
+  });
+
+};
+
+module.exports = {
+  signUp,
+  signIn,
+  logout,
+};
