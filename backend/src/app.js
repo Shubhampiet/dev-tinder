3e04611c0983f72e075654b38f0d53a58ebd63a2
Shubham Singh <shubham.itpiet@gmail.com>
2026-07-08 21:16:58 +0530

    auth middleware

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
 
+const authMiddleware = require("./middlewares/auth.middleware");
 const authRoutes = require("./routes/auth.routes");
 const profileRoutes = require("./routes/profile.routes");
+const connectionRoutes = require("./routes/connectionRequest.routes");
+const feedRoutes = require("./routes/feed.route");
 
 const app = express();
 app.use(cookieParser());
@@ -11,6 +14,7 @@ app.use(express.json());
 
 //routes
 app.use("/api/auth", authRoutes);
-app.use("/api/profile", profileRoutes);
-
+app.use("/api/profile", authMiddleware, profileRoutes);
+app.use("/api/connection", authMiddleware, connectionRoutes);
+app.use("/api", authMiddleware, feedRoutes);
 module.exports = app;
