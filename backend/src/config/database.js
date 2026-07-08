cde8e68cb9bf0d3b3f5c3eb71ec756aa5367ac8c
Shubham Singh <shubham.itpiet@gmail.com>
2026-07-08 21:17:51 +0530

    mongo url changes

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDb = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.Mongo_URI);
     console.log("Connected to the database successfully");
   } catch (error) {
     console.error("Error connecting to the database:", error);
