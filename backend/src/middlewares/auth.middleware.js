0416fa475aef7e52c3ea09bdacaf92bac13f6375
Shubham Singh <shubham.itpiet@gmail.com>
2026-07-17 21:46:14 +0530

    auth middleware

--- backend/src/middlewares/auth.middleware.js
+++ backend/src/middlewares/auth.middleware.js
@@ -2,13 +2,13 @@ const jwt = require("jsonwebtoken");
 const User = require("../models/user.model");
 
 const authMiddleware = async (req, res, next) => {
-  const { token } = req.cookies;
+  const { token } = req.cookies.accessToken;
   if (!token) {
     return res.status(401).json({ error: "Unauthorized" });
   }
   //verify token
   try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
     const user = await User.findById(decoded.id);
     if (!user) {
       return res.status(401).json({ error: "Unauthorized" });
