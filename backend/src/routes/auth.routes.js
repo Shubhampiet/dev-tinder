f075544148c52c7d70be1a33684d2aadcafb0611
Shubham Singh <shubham.itpiet@gmail.com>
2026-07-18 07:18:34 +0530

    auth routes

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { signUp, signIn, logout } = require("../controllers/auth.controller");
+const { signUp, signIn, logout, refreshToken } = require("../controllers/auth.controller");
 const authMiddleware = require("../middlewares/auth.middleware");
 
 router.post("/signup", signUp);
 router.post("/signin", signIn);
 router.post("/logout", authMiddleware, logout);
+router.post("/refresh", refreshToken);
 
 module.exports = router;
